50076c7c1a55faa517ebcb1677c797122d24a73e
Nathan Parker <30416735+nbparker@users.noreply.github.com>
2020-07-27 23:33:56 +0100

    comments

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,14 +3,16 @@
 
 def fizz_buzz():
     for num in range(1, 101):
+        # really good for readability and code maintenance - where'd you pick that up
         fizzing = num % 3 == 0
         buzzing = num % 5 == 0
-        if fizzing and not buzzing:
-            print('Fizz')
-        elif buzzing and not fizzing:
-            print('Buzz')
-        elif fizzing and buzzing:
+        # slight clean up for less equality checks
+        if fizzing and buzzing:
             print('FizzBuzz')
+        elif fizzing:
+            print('Fizz')
+        elif buzzing:
+            print('Buzz')
         else:
             print(num)
 
