a3b321dc22c7eb65228db96f52ec02016b81307d
borisdepfeffel <66782145+borisdepfeffel@users.noreply.github.com>
2020-07-27 12:49:36 +0100

    Add files via upload

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,13 @@
 
+
+
 def fizz_buzz():
     for num in range(1, 101):
         fizzing = num % 3 == 0
         buzzing = num % 5 == 0
         if fizzing and not buzzing:
             print('Fizz')
-        elif num % 5 == 0 and not fizzing:
+        elif buzzing and not fizzing:
             print('Buzz')
         elif fizzing and buzzing:
             print('FizzBuzz')
