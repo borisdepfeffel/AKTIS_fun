4ca1628ff9996bc1e598794c8985678d0d1a9ce7
borisdepfeffel <66782145+borisdepfeffel@users.noreply.github.com>
2020-07-29 15:39:17 +0100

    Add files via upload

--- BinaryDivmod.py
+++ BinaryDivmod.py
@@ -1,26 +1,20 @@
 
-n = 450400493
+n = 3
 print(bin(n)[2:])
 
 def int_to_bin(n, lst=None):
     if lst is None:
         lst = []
-    while n >= 0:
-        if n == 1:
-            lst.append(1)
-            break
-        elif n == 0:
-            lst.append(0)
-            break
-        else:
-            for i in range(500,0,-1):
-                x = 2 ** i
-                p, q = divmod(n, x)
-                if p == 0 and len(lst) > 0:
-                    lst.append(p)
-                if p == 1:
-                    n -= n - q
-                    lst.append(p)
+    while n > 1:
+        for i in range(500,0,-1):
+            x = 2 ** i
+            p, q = divmod(n, x)
+            if p == 0 and len(lst) > 0:
+                lst.append(p)
+            if p == 1:
+                n -= n - q
+                lst.append(p)
+    lst.append(n)
     result = ''.join([str(x) for x in lst])
     return result
 
